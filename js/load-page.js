1cf74edd39357f8bd61b64c8f2ca36ba6313afa0
maria julia <majuthelink@gmail.com>
2021-08-16 11:41:42 +0000

    changed jsload/p2

--- js/load-page.js
+++ js/load-page.js
@@ -7,6 +7,34 @@ $(document ).ready(function(){
     $('.modal-tittle').empty()
     $('.modal-body').empty()
 
-    alert(page)
+  switch(page){
+    case 'about':
+      $('.modal-tittle').append('Sobre')
+      $('.modal-body').append(´<p>lorem</p>´)
+      $('#modal-info').modal('show')
+      break
+
+    case 'work':
+      $('.modal-tittle').append('Projetos')
+      $('.modal-body').append('meus trabalhos')
+      $('#modal-info').modal('show')
+      break
+
+    case 'client':
+      $('.modal-tittle').append('Parceiros')
+      $('.modal-body').append('meus parceiros')
+      $('#modal-info').modal('show')
+      break
+
+    case 'contact':
+      $('.modal-tittle').append('Contato')
+      $('.modal-body').append('entre em contato')
+      $('#modal-info').modal('show')
+      break  
+      default: alert('link não encontrado')
+
+      
+      
+  }
   })
 })
